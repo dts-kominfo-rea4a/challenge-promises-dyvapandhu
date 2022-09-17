0859ab15ba67e515d7267ed8ecaddaae12d702fa
dyvapandhu <dyvapandhu989@gmail.com>
2022-09-17 13:32:18 +0700

    [CHANGE] Modify promise.js

--- promise.js
+++ promise.js
@@ -1,7 +1,18 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (category) => {
+  let result1 = await promiseTheaterIXX();
+  let result2 = await promiseTheaterVGC();
+
+  result1 = result1.filter(
+    (val) => val.hasil.toLowerCase() === category.toLowerCase()
+  );
+  result2 = result2.filter(
+    (val) => val.hasil.toLowerCase() === category.toLowerCase()
+  );
+  return result1.length + result1.length;
+};
 
 module.exports = {
   promiseOutput,
